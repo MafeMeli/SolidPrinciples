d7476236eb1db2bc2f9708daaf05872fa6a8bc26
MafeMeli <maria.vargas@mercadolibre.com.co>
2022-12-05 02:22:26 -0500

    OpenClose Principle solved

--- app/src/main/java/com/example/solidprinciples/data/FileLogger.kt
+++ app/src/main/java/com/example/solidprinciples/data/FileLogger.kt
@@ -6,17 +6,35 @@ import java.io.File
  * Ésta clase tiene la única responsabilidad de loggear el error.
  * La O en SOLID corresponde a Open for extention but closed for modification.
  */
-class FileLogger {
+open class FileLogger {
 
 
     // Que pasa si queremos cambiar la manera en que logueamos el error?
-    fun logError(error: String){
+    open fun logError(error: String){
         // En caso de error lo guardamos en un archivo de error
         // Cambiar simplemente el nombre del archivo estaría mal, podría generar conflictos.
-        val file = File("error2.txt")
+        // Porque donde quiera que usemos esta clase en nuestro código, cambiaría la manera en que
+        // logueamos nuestros errores y tal vez no es lo que queremos.
+        // No queremos afectar to do el codigo con sólo un cambio
+        val file = File("error.txt")
         file.appendText(
             text = error
         )
     }
 
+}
+
+class CustomFileLogger: FileLogger() {
+
+    /**
+     * Podemos crear una clase nueva que extienda de una clase padre
+     * y modificar su comportamiento para que se ajuste a lo que necesitamos
+     * sin modificar a la clase padree.
+     */
+    override fun logError(error: String) {
+        val file = File("my_custom_error_file.txt")
+        file.appendText(
+            text = error
+        )
+    }
 }
